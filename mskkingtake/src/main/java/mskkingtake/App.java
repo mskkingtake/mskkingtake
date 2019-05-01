415ae759952b0fe1eea83e69d690041ac161696a
mskkingtake <mskkingtake@vip.163.com>
2019-05-01 11:50:03 +0800

    个人使用初版提交
    个人使用初版提交

---
+++ mskkingtake/src/main/java/mskkingtake/App.java
@@ -0,0 +1,13 @@
+package mskkingtake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
